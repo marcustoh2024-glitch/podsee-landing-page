576a102c7e99206df445b43fb57403f66864ce34
Marcus Toh <marcus@MacBook-Pro.local>
2026-01-31 20:56:57 +0800

    Auto-save: 2026-01-31 20:56:57

--- src/components/PageShell.jsx
+++ src/components/PageShell.jsx
@@ -4,7 +4,7 @@ import ScrollProgress from './ScrollProgress'
 
 export default function PageShell({ children }) {
   return (
-    <div className="h-screen overflow-hidden relative bg-[#F5F1E8] lg:bg-surface lg:min-h-screen lg:h-auto lg:overflow-auto">
+    <div className="min-h-screen overflow-y-auto relative bg-[#F5F1E8] lg:bg-surface lg:overflow-auto">
       {/* Background layer (z-0) - Only on desktop */}
       <div className="hidden lg:block">
         <AnimatedBackground />
@@ -14,9 +14,9 @@ export default function PageShell({ children }) {
       <ScrollProgress />
       
       {/* Content layer (z-10) */}
-      <div className="relative z-10 h-full flex flex-col lg:h-auto lg:max-w-none lg:px-0">
+      <div className="relative z-10 flex flex-col lg:h-auto lg:max-w-none lg:px-0">
         {/* Main content area */}
-        <main className="flex-1 min-h-0 overflow-hidden lg:py-0 lg:overflow-visible lg:min-h-fit">
+        <main className="flex-1 lg:py-0 lg:overflow-visible lg:min-h-fit">
           {children}
         </main>
       </div>
